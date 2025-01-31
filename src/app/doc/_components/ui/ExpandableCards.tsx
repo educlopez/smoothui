c052fb894e1ad5b261c7f37143d024dce52cc1d8
Edu Calvo <educlopez93@gmail.com>
2025-01-31 12:18:15 +0100

    feat: replace external image URLs with local imports in ExpandableCards component
    - Updated image sources for Summer Opening, Fashion, Gallery Art, and Dreams cards to use local assets.

--- src/app/doc/_components/ui/ExpandableCards.tsx
+++ src/app/doc/_components/ui/ExpandableCards.tsx
@@ -5,6 +5,11 @@ import Image from "next/image"
 import { Play } from "lucide-react"
 import { AnimatePresence, motion } from "motion/react"
 
+import dreams from "@/app/doc/_components/resources/expandablecards/dreams.webp"
+import fashion from "@/app/doc/_components/resources/expandablecards/fashion.webp"
+import galleryart from "@/app/doc/_components/resources/expandablecards/galleryart.webp"
+import summer from "@/app/doc/_components/resources/expandablecards/summer.webp"
+
 interface Card {
   id: number
   title: string
@@ -20,8 +25,7 @@ const cards: Card[] = [
   {
     id: 1,
     title: "Summer Opening",
-    image:
-      "https://www.lummi.ai/api/render/image/327e46ae-cca2-4c02-9b9f-b3272f943324?token=eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjMyN2U0NmFlLWNjYTItNGMwMi05YjlmLWIzMjcyZjk0MzMyNCIsImRvd25sb2FkU2l6ZSI6Im1lZGl1bSIsInJlbmRlclNwZWNzIjp7ImVmZmVjdHMiOnsicmVmcmFtZSI6e319fSwic2hvdWxkQXV0b0Rvd25sb2FkIjpmYWxzZSwianRpIjoiWlhjMl9Yd2FJY0w1ZDZ3V3dDdWxuIiwiaWF0IjoxNzM4MjY3NTI1LCJleHAiOjE3MzgyNjc1ODV9.BGSD_ANj91L-P6H_5VptMaSgHoQ9nL_lfx3buympcE8",
+    image: summer.src,
     content:
       "Join us for the Summer Opening event, where we celebrate the start of a vibrant season filled with art and culture.",
     author: {
@@ -32,8 +36,7 @@ const cards: Card[] = [
   {
     id: 2,
     title: "Fashion",
-    image:
-      "https://www.lummi.ai/api/render/image/2a19122c-0942-447f-8f84-490bf5ad75ab?token=eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjJhMTkxMjJjLTA5NDItNDQ3Zi04Zjg0LTQ5MGJmNWFkNzVhYiIsImRvd25sb2FkU2l6ZSI6Im1lZGl1bSIsInJlbmRlclNwZWNzIjp7ImVmZmVjdHMiOnsicmVmcmFtZSI6e319fSwic2hvdWxkQXV0b0Rvd25sb2FkIjpmYWxzZSwianRpIjoiY3Z6bURUVTJCcXhPd0VTVHh1WmFZIiwiaWF0IjoxNzM4MjY3NTY0LCJleHAiOjE3MzgyNjc2MjR9.ZWR_b1uD4KDdRUTTei1aO9VWxdfgLLOL6CNzqtQrGbY",
+    image: fashion.src,
     content:
       "Explore the latest trends in fashion at our exclusive showcase, featuring renowned designers and unique styles.",
     author: {
@@ -44,8 +47,7 @@ const cards: Card[] = [
   {
     id: 3,
     title: "Gallery Art",
-    image:
-      "https://www.lummi.ai/api/render/image/098aec2d-542c-4124-8fca-33ac81e98d81?token=eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjA5OGFlYzJkLTU0MmMtNDEyNC04ZmNhLTMzYWM4MWU5OGQ4MSIsImRvd25sb2FkU2l6ZSI6Im1lZGl1bSIsInJlbmRlclNwZWNzIjp7ImVmZmVjdHMiOnsicmVmcmFtZSI6e319fSwic2hvdWxkQXV0b0Rvd25sb2FkIjpmYWxzZSwianRpIjoiNWZUbXZCVm05TXJRUFptTzVySVYzIiwiaWF0IjoxNzM4MjY2OTAwLCJleHAiOjE3MzgyNjY5NjB9.mxH3AqSsa3E7MP-ogk07ZqRm1_Ayejx5a-styp6tRaI",
+    image: galleryart.src,
     content:
       "Immerse yourself in the world of art at our gallery, showcasing stunning pieces from emerging and established artists.",
     author: {
@@ -56,8 +58,7 @@ const cards: Card[] = [
   {
     id: 4,
     title: "Dreams",
-    image:
-      "https://www.lummi.ai/api/render/image/ee59a648-48ef-45cf-8eef-b1580308331d?token=eyJhbGciOiJIUzI1NiJ9.eyJpZCI6ImVlNTlhNjQ4LTQ4ZWYtNDVjZi04ZWVmLWIxNTgwMzA4MzMxZCIsImRvd25sb2FkU2l6ZSI6Im1lZGl1bSIsInJlbmRlclNwZWNzIjp7ImVmZmVjdHMiOnsicmVmcmFtZSI6e319fSwic2hvdWxkQXV0b0Rvd25sb2FkIjpmYWxzZSwianRpIjoibk1jTVFSUHpnNEZMd1hLX3ZNdGpuIiwiaWF0IjoxNzM4MjY2OTIzLCJleHAiOjE3MzgyNjY5ODN9.RRl6ZbXGLz7MiiWR1oCudynBnV7DQxeLI1PIAKuP4Ew",
+    image: dreams.src,
     content:
       "Join us on a journey through dreams, exploring the subconscious and the art of dreaming.",
     author: {
