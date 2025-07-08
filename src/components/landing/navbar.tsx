cb2e7a6e123bbf34c623927d7de1400050b42c79
Edu Calvo <educlopez93@gmail.com>
2025-07-08 19:25:43 +0200

    style: update Navbar component for improved layout and responsiveness
    - Adjusted the gap between elements in the Navbar for better spacing on different screen sizes.
    - Modified the Logo component's size and text properties to enhance visual appeal and consistency across devices.
    - Improved the alignment of items in the Navbar for a more polished look.

--- src/components/landing/navbar.tsx
+++ src/components/landing/navbar.tsx
@@ -8,10 +8,10 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 right-0 left-0 z-40 flex h-16 items-center justify-between py-2">
       <div className="mx-auto flex w-full max-w-7xl flex-row items-center justify-between px-4">
-        <div className="flex items-center justify-center gap-3">
-          <Logo classNameIcon="w-6" className="w-32" />
+        <div className="flex items-center justify-center gap-2 md:gap-3">
+          <Logo classNameIcon="w-4 md:w-6" className="text-xl md:text-3xl" />
         </div>
-        <div className="flex items-center gap-6">
+        <div className="flex items-center justify-end gap-4 md:gap-6">
           <Link
             href="/doc"
             className="hover:text-brand text-sm font-medium transition-colors"
