80b809ba9d0280caff99f5b710b46fcd6aec0c90
Edu Calvo <educlopez93@gmail.com>
2025-11-03 21:37:48 +0100

    feat: add footer components with simple and complex layouts
    - Introduced two new footer components: FooterSimple and FooterComplex, each providing different layouts and functionalities.
    - Updated pnpm-lock.yaml to include dependencies for the new footer components.
    - Created example files for both footer components in the documentation.
    - Added necessary TypeScript configurations and package.json files for each footer block.

--- packages/smoothui/blocks/index.ts
+++ packages/smoothui/blocks/index.ts
@@ -1,4 +1,6 @@
 // Export all UI blocks from their individual packages
+export { default as Footer1 } from "./footer-1";
+export { default as Footer2 } from "./footer-2";
 export { default as Header1 } from "./header-1";
 export { default as Header2 } from "./header-2";
 export { default as Header3 } from "./header-3";
