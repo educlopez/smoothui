a6652d455bc718ea441f9fa33d9c3ee45048a5db
Edu Calvo <educlopez93@gmail.com>
2026-01-29 12:16:53 +0100

    feat(blocks): add footer-3 mega footer with newsletter signup

---
+++ apps/docs/examples/footer-3.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Footer3 from "@repo/smoothui/blocks/footer-3";
+
+const Example = () => (
+  <div className="h-full w-full">
+    <Footer3 />
+  </div>
+);
+
+export default Example;
